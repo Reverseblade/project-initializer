c5edd39569220a4ba483aef1a704d068e3df9450
Reverseblade <fujisawa_y@uuum.jp>
2019-07-20 04:47:54 +0900

    add config.py

---
+++ github_repository_initializer/config.py
@@ -0,0 +1,7 @@
+GITHUB_USERNAME = 'your github username'
+GITHUB_PASSWORD = 'your github password'
+
+GITHUB_LOGIN_PAGE_URL = 'http://github.com/login'
+GITHUB_CREATE_REPOSITORY_PAGE_URL = 'https://github.com/new'
+
+IS_HEADLESS = True
